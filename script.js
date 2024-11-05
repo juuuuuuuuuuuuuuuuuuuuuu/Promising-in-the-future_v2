4e996b7ee5926e6d3adda39d725fbbb8a1b46f74
juuuuuuuuuuuuuuuuuuuuuu <bugsmobile8986@gmail.com>
2024-11-05 11:53:19 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const images = [
     'image2.jpg', // 두 번째 이미지
     'image3.jpg'  // 세 번째 이미지
 ];
+const lnk = [
+    'https://juuuuuuuuuuuuuuuuuuuuuu.github.io/swjb_v2/', // 첫 번째 이미지
+    'https://juuuuuuuuuuuuuuuuuuuuuu.github.io/Promising-in-the-future_v2/', // 첫 번째 이미지
+    'https://en.wikipedia.org/wiki/C_(programming_language)', // 첫 번째 이미지
+];
 
 let currentIndex = 0;
 
@@ -10,8 +15,14 @@ let currentIndex = 0;
 function updateImage() {
     const slide = document.getElementById('slide');
     const imageIndex = document.getElementById('imageIndex');
-    slide.src = images[currentIndex];
+    const slideLink = document.getElementById("slideLink");
+
     imageIndex.textContent = `${currentIndex + 1} / ${images.length}`;
+     
+    slideLink.setAttribute("href",`${lnk[currentIndex]}`)
+    slide.src = images[currentIndex];
+
+    console.log(currentIndex)
 }
 
 // 이전 버튼 클릭 이벤트
